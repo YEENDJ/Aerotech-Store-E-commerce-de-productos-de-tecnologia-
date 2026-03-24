80341325c92ec07ab870cb5cade907264698237e
yeendj <yeendj@gmail.com>
2026-03-24 17:15:56 -0500

    ajuste responsivo en componente productlist

--- front/src/components/ProductsList.tsx
+++ front/src/components/ProductsList.tsx
@@ -13,27 +13,13 @@ const ProductsList = ({ products }: ProductsListProps) => {
         Nuestros Productos
       </h1>
 
-      <div className="flex flex-wrap justify-center gap-10 max-w-6xl mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8 lg:gap-10 max-w-7xl mx-auto px-4 justify-items-center">
         {products.map(product => (
           <div
             key={product.id}
-            className="
-              w-full 
-              sm:w-[90%] 
-              md:w-[45%] 
-              lg:w-[30%] 
-              xl:w-[30%] 
-              flex 
-              justify-center
-              transform
-              hover:scale-[1.03]
-              transition-transform
-              duration-300
-            "
+            className="w-full flex justify-center transform hover:scale-[1.03] transition-transform duration-300"
           >
-            <div className="w-full max-w-[360px]">
-              <ProductCard product={product} />
-            </div>
+            <ProductCard product={product} />
           </div>
         ))}
       </div>
